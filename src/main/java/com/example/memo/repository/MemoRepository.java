b28f25454dbd55a2a84f7be9ba744e766b9c8ca6
doot00 <binnung_s@naver.com>
2024-10-21 16:01:03 +0900

    JPA
    SpringBoot JPA 사용

--- src/main/java/com/example/memo/repository/MemoRepository.java
+++ src/main/java/com/example/memo/repository/MemoRepository.java
@@ -3,12 +3,14 @@ package com.example.memo.repository;
 import com.example.memo.dto.MemoRequestDto;
 import com.example.memo.dto.MemoResponseDto;
 import com.example.memo.entity.Memo;
+import jakarta.persistence.EntityManager;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.RowMapper;
 import org.springframework.jdbc.support.GeneratedKeyHolder;
 import org.springframework.jdbc.support.KeyHolder;
 import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
@@ -92,5 +94,14 @@ public class MemoRepository {
                 return null;
             }
         }, id);
+    }
+    @Transactional
+    public Memo createMemo(EntityManager em) {
+        Memo memo = em.find(Memo.class, 1);
+        memo.setUsername("Robbert"); // 데이터를 변경해준다.
+        memo.setContents("@Transactional 전파 테스트 중!"); // 업데이트 쿼리가 생성된다.
+
+        System.out.println("createMemo 메서드 종료");
+        return memo;
     }
 }
